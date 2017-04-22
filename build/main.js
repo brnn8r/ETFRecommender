9a2cbd507d89cc7787b7abf767218b3c533223b5
Stephen Christopher Adams <vengeanz@gmail.com>
2017-04-23 09:36:22 +1200

    Removed build folder
    Removed build folder and got script running working

--- build/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ETFRecommender_1 = require("./ETFRecommender");
-var recommender = new ETFRecommender_1.ETFRecommender(LoadConfiguration());
-recommender.Start();
-function LoadConfiguration() {
-    return { tolerance: 0.05, exchangeTradedFunds: ["ASP"] };
-}
-//# sourceMappingURL=main.js.map
